3c6339724dc5bd01893e7bd435d2e7247b5b9c63
STT-Seah <82761806+STT-Seah@users.noreply.github.com>
2021-07-04 23:46:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@
                                                                                       print("bye")
                                                                                                
                                                                                                                                                                                       
-                                                                                           print(hello())
+                                                                                               print(hello())
