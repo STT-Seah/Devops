286615b412acec39c7ebf44cf7e4a6dbbd887d4d
STT-Seah <82761806+STT-Seah@users.noreply.github.com>
2021-07-04 23:52:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
                                                                                                                                   def hello():
-                                                                                                                          print("hi")
+                                                                                                                          p rint("hi")
                                                                                                                                      
 
                                                                                                                          def bye():
